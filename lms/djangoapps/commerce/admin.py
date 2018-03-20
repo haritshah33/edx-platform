0e86a6a4f1ba41f20e7103f10709b4b32d918372
Harit <harit.shah@engineer.ai>
2018-03-20 15:27:56 +0530

    Removed

--- lms/djangoapps/commerce/admin.py
+++
@@ -1,8 +0,0 @@
-""" Admin site bindings for commerce app. """
-
-from django.contrib import admin
-
-from commerce.models import CommerceConfiguration
-from config_models.admin import ConfigurationModelAdmin
-
-admin.site.register(CommerceConfiguration, ConfigurationModelAdmin)
--- openedx/core/djangoapps/content/block_structure/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-This code exists in openedx/core/djangoapp because it needs access to django signaling mechanisms
-
-Most of the underlying functionality is implemented in openedx/core/lib/block_structure/
-"""
